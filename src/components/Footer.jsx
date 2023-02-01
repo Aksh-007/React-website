c2b25faa78fd580f7b5b874f31cf9433cf15af44
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-01 20:48:38 +0530

    added components ans scss

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.instagram.com/akshaybarapatre_aksh/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Aksh-007" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
